0064162135cc7a575b0e0b3144395f272c7eb322
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2021-11-19 23:44:36 +0900

    カリキュラム08-04やり直し

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,12 +8,15 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
-    
     {
-    return $post->get();
+        return view('index')->with(['posts' => $post->getPaginate()]);
+    }
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
     }
     public function create()
     {
-    return view('create');
+        return view('create');
     }
 }
