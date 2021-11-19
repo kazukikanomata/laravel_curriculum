8120f1158c509a5df9348768489cde548c59f1eb
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2021-11-19 23:54:23 +0900

    カリキュラム08-03やり直し

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,33 +9,12 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('index')->with(['posts' => $post->getPaginate()]);
     }
+    
     // カリキュラム08-3ブログ投稿詳細の表示
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function create()
-    {
-        return view('create');
-    }
-    public function store(Request $request, Post $post)
-    {
-        $input = $request['post'];
-        $post->fill($input)->save();
-        return redirect('/posts/' . $post->id);
-    }
-    public function edit(Post $post)
-    {
-    return view('posts/edit')->with(['post' => $post]);
-    }
-    public function update(PostRequest $request, Post $post)
-    {
-    $input_post = $request['post'];
-    $post->fill($input_post)->save();
-
-    return redirect('/posts/' . $post->id);
-    }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() {
-    return view('posts/index');
-});
-Route::get('/posts/{post}', 'PostController@show');
-
 Route::get('/', 'PostController@index');
-Route::get('/posts/create', 'PostController@create');
-Route::post('/posts', 'PostController@store');
-Route::get('/posts/{post}/edit', 'PostController@edit');
-Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}', 'PostController@show');
