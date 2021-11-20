5df95d8e3c70df7eaee4d4db4475c5c31cc49fdd
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2021-11-20 11:27:03 +0900

    show.blade.phpに追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,1 +1,24 @@
-<p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+    </head>
+    <body>
+       <h1>Blog Name</h1>
+           <div class='post'>
+               <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+               <h2 class='title'>{{ $post->title }}</h2>
+               <p class='body'>{{ $post->body }}</p>
+               <p class='updated_at'>{{ $post->updated_at }}</p>
+           </div>
+           <div class='back'>[<a href='/'>back</a>]</div>
+    </body>
+</html>
