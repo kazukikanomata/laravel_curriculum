84210f329aad5922d00058847ab0bca1f166c2da
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2021-11-20 11:41:54 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',function(){
-    return view('welcome');
-});
+
 Route::get('/posts', 'PostController@index');
 Route::get('/', 'PostController@index');
